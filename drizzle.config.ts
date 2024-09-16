f4972f64bd5b7105caeb273c097a8eae4a452b7f
Johnny <johnny_dmg@hotmail.com>
2024-09-16 19:02:08 -0300

    fix

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'drizzle-kit'
+import { env } from './src/env'
+
+export default defineConfig({                   //configuracao base foi pega na documentacao do drizzle
+        schema: "./src/db/schema.ts",                  //foram feito modificacoes     
+        out:'./.migrations',    //pasta onde fica arquivos de migration(define linha do tempo do BD)
+        dialect: 'postgresql',  //seleciona qual BD
+        dbCredentials:{         //url postgre pego no site Neon DB Online
+            url: env.DATABASE_URL,
+        },
+    })
+    //OBS url: process.env.DATABASE_URL foi substituido por env.DATABASE_URL,
+    //url em .Env 'postgresql://neondb_owner:ix2XHvrFUC9E@ep-shiny-sun-a56f4y8w.us-east-2.aws.neon.tech/neondb?sslmode=require'
+    //caso utiliza-se docker: em url seria:
+    //'postgresql://docker:docker@localhost:5432/inorbit' // aqui tem dados das credenciais e nome  db
+    //porem nao é boa pratica utilizar dessa forma, e normalmente utiliza variaveis de ambiente
+    //serao configuradas no arquivo.env
---
+++ src/db/schema.ts
@@ -0,0 +1,28 @@
+//Drizzle é uma ferramenta de migração e gerenciamento de esquemas para bancos de dados relacionais
+
+import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
+
+export const goals = pgTable('goals', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  title: text('title').notNull(),
+  desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
+  //campo da tabela numero de vezes (que deseja praticar meta x (regra de negocio))
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
+
+export const goalCompletions = pgTable('goal_completions', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  goalId: text('goal_id')
+    .references(() => goals.id)
+    .notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
